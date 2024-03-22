433b562d2ce78e80ed89f895c07f4fafc9750376
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-22 10:24:41 -0700

    This will be the main code for the tree

---
+++ rbtree.cpp
@@ -0,0 +1,16 @@
+//C++ Data Structures: Red Black Tree - Akhil Baidya
+//Submission Date: 5/2/24
+/*
+Notes: This is a self-balancing Binary Tree that takes integers from 1-999. It can add them manually or through a file and print out the values.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include "node.h"
+
+using namespace std;
+
+int main() {
+  return 0;
+}
