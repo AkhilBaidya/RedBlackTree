abb89ff062b6e9e7edda4f0ea58764c2c762f493
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-22 10:41:28 -0700

    Editing loop

--- rbtree.cpp
+++ rbtree.cpp
@@ -25,6 +25,40 @@ void check(node*);
 
 /*In this main function, the user will be able to input commands to edit the Red Black Tree (adding values to it and printing it)*/
 int main() {
+
+  node* head = new node();
+  head = NULL; //initial head of tree
+
+  bool editing = true; //loops the command input process
+  char command[15]; //user command
+
+  while (editing) {
+
+    cout << "What would you like to do? ADD to tree? PRINT tree? QUIT?" << endl;
+    cin >> command;
+
+    if (!strcmp(command, "ADD")) {
+    }
+
+    else if (!strcmp(command, "PRINT")) {
+
+    }
+    
+    else if (!strcmp(command, "QUIT")) {
+      editing = false;
+      cout << "Quitting..." << endl;
+    }
+
+    else {
+      cout << "Didn't understand that." << endl;
+      cout << "_          _" << endl;
+      cout << " \_(0_0)?_/" << endl;
+    }
+    
+  }
+
+  cout << "Thank you for witnessing this Red Black Tree program!" << endl;
+  
   return 0;
 }
 
