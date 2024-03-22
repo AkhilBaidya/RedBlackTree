3cdc204eda6907bee76e1a8236f0c3c49c771c16
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-22 10:13:57 -0700

    Defining node class

---
+++ node.cpp
@@ -0,0 +1,12 @@
+//C++ Data Structures: Node Class Definitions - Akhil Baidya
+//Submission Date: 3/22/2024
+/*
+Notes: This file defines the get and set functions (for the integer data, color, parent, left child, and right child of the node), constructor, and destructor of the node class.
+*/
+
+#include <iostream>
+#include <cstring>
+#include "node.h"
+
+using namespace std;
+
