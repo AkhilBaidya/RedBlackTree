4dfe1c65b95c2881de5c9e7d0dbe3649b41eb434
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-22 10:15:43 -0700

    Defining node class constructor and destructor

--- node.cpp
+++ node.cpp
@@ -10,3 +10,26 @@ Notes: This file defines the get and set functions (for the integer data, color,
 
 using namespace std;
 
+//Constructors
+node::node() {
+  parent = NULL;
+  left = NULL;
+  right = NULL;
+  data = 0;
+  color = 'R';
+}
+
+node::node(int input) {
+  parent = NULL;
+  left = NULL;
+  right = NULL;
+  data = input;
+  color = 'R';
+}
+
+//Destructor
+node::~node() {
+  delete parent;
+  delete left;
+  delete right;
+}
