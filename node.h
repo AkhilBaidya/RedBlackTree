6add4fbc13e799a111c581a75c4c208a70fc56d8
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-22 10:08:50 -0700

    Making node class

---
+++ node.h
@@ -0,0 +1,17 @@
+//C++ Data Structures: Node Class Header File - Akhil Baidya
+//Submission Deadline: 3/22/2024
+/*
+Notes: The Node Class will have a pointer to its parent and to its left and right children. It will also contain integer data. 
+There will be get and set functions for all of these values, on top of a constructor and destructor.
+*/
+
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class node {
+ public:
+ private:
+};
