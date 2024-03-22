e79b661c1a4c0eb325c6b075de9160ae4aeaddc2
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-22 10:11:13 -0700

    Making node class

--- node.h
+++ node.h
@@ -1,7 +1,7 @@
 //C++ Data Structures: Node Class Header File - Akhil Baidya
 //Submission Deadline: 3/22/2024
 /*
-Notes: The Node Class will have a pointer to its parent and to its left and right children. It will also contain integer data. 
+Notes: The Node Class will have a pointer to its parent and to its left and right children. It will also contain integer data and a color. 
 There will be get and set functions for all of these values, on top of a constructor and destructor.
 */
 
@@ -12,6 +12,29 @@ There will be get and set functions for all of these values, on top of a constru
 using namespace std;
 
 class node {
+
  public:
+
+  //constructors:
+  node();
+  node(int);
+
+  //destructor:
+  ~node();
+
+  //set functions:
+  void setPar(node*);
+  void setL(node*);
+  void setR(node*);
+  void setData(int);
+  void setColor(char);
+
+  //get functions:
+  node* getPar();
+  node* getL();
+  node* getR();
+  int getData();
+  char getcolor();
+  
  private:
 };
