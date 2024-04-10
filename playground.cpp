8286a99dbc8d88246386c62d6fb25cfd1c025963
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-04-10 10:40:26 -0700

    Copied over main function code from previous version of rbtree.cpp into this file

--- playground.cpp
+++ playground.cpp
@@ -6,6 +6,42 @@ using namespace std;
 
 int main() {
 
+  //Code from previous version of rbtree.cpp (just copied it over and will edit it)
+  node* head = new node();
+  head = NULL; //initial head of tree
+
+  bool editing = true; //loops the command input process
+  char command[15]; //user command
+
+  while (editing) {
+
+    cout << "What would you like to do? ADD to tree? PRINT tree? QUIT?" << endl;
+    cin >> command;
+
+    if (!strcmp(command, "ADD")) {
+    }
+
+    else if (!strcmp(command, "PRINT")) {
+      if (head == NULL) {
+        cout << "There is no tree ~ <3" << endl
+      }
+
+      else {
+        cout << "Here's the tree:" << endl;
+        print(head, 0);
+      }
+    }
+
+    else if (!strcmp(command, "QUIT")) {
+      editing = false;
+      cout << "Quitting..." << endl;
+    }
+
+    else {
+      cout << "Didn't understand that." << endl;
+      cout << "_          _" << endl;
+      cout << " \_(0_0)?_/" << endl;
+    }
 
   return 0;
 }
