d3cdb7a537c6e01f07a861032d07ee994baf4878
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-04-10 09:37:55 -0700

    Reformatting code such that rbtree is a class

---
+++ rbtree.h
@@ -0,0 +1,11 @@
+#ifndef RBTREE_H
+#define RBTREE_H
+
+//Header file for rbtree
+//Header guards from https://www.learncpp.com/cpp-tutorial/header-guards/
+#include <iostream>
+#include <cstring>
+#include "node.h"
+
+
+#endif
