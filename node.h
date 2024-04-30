838fad7406480be24a44c276455ea313dbf83fff
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-04-30 09:38:09 -0700

    Final Comments

--- node.h
+++ node.h
@@ -2,11 +2,15 @@
 #define NODE_H
 
 //C++ Data Structures: Node Class Header File - Akhil Baidya
-//Submission Deadline: 3/22/2024
+//Submission Deadline: 5/2/2024
+
 /*
-Notes: The Node Class will have a pointer to its parent and to its left and right children. It will also contain integer data and a color. 
-There will be get and set functions for all of these values, on top of a constructor and destructor.
-Header guards from https://www.learncpp.com/cpp-tutorial/header-guards/
+Notes: 
+The Node Class will have a pointer to its parent and to its left and right children. It will also contain integer data and a color.
+There will be get and set functions for all of these values, as well as a constructor and destructor.
+
+Sources:
+Referred to https://www.learncpp.com/cpp-tutorial/header-guards/, a tutorial provided by user "Alex" from the website "Learn C++" for header guard syntax (since node.h will be called multiple times, this helps prevent redefinition).
 */
 
 
@@ -19,21 +23,21 @@ class node {
 
  public:
 
-  //constructors:
+  //Constructors:
   node();
   node(int);
 
-  //destructor:
+  //Destructor:
   ~node();
 
-  //set functions:
+  //Set functions:
   void setPar(node*);
   void setL(node*);
   void setR(node*);
   void setData(int);
   void setColor(char);
 
-  //get functions:
+  //Get functions:
   node* getPar();
   node* getL();
   node* getR();
