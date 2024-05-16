546a9a3269c6259222b5cfdf49da76b5c722e540
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-05-16 09:40:42 -0700

    Final Version

--- rbtree.h
+++ rbtree.h
@@ -6,7 +6,7 @@
 
 /*
 Notes:
-The Red Black Tree Class possesses several functions that add to (add), delete from (del), print from (print), and manipulate the tree (private functions called after "add"). It also has a constructor and destructor, as a class.
+The Red Black Tree Class possesses several functions that add to (add), delete from (del), print from (print), search in (search), and manipulate the tree (private functions called after "add"). It also has a constructor and destructor, as a class.
 
 Sources:
 Referred to https://www.learncpp.com/cpp-tutorial/header-guards/, a tutorial provided by user "Alex" from the website "Learn C++" for header guard syntax. 
@@ -36,6 +36,9 @@ public:
 
   //Delete from Tree:
   void del(int);
+
+  //Search in Tree:
+  void search(int);
   
 private:
   //Key Data:
@@ -61,6 +64,8 @@ private:
   void rotLR(node*); //a Left-Right rotation
   void rotRL(node*); //a Right-Left rotation
   void rotRR(node*); //a Right-Right rotation
+
+  //Other (for recursive purposes):
   void recPrint(node*, int); //called by the print function to recursively go through tree
 };
 
